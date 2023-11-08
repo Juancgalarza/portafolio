f534b29854a594c791242a2c5f351db35b974375
JuanCa <juanca_20201@outlook.es>
2023-11-08 00:50:26 -0500

    template completo sin rutas ni animaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { CounterComponent } from './pages/counter/counter.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,13 @@ import { AboutComponent } from './pages/about/about.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    ServicesComponent,
+    CounterComponent,
+    PortafolioComponent,
+    TestimonialsComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,

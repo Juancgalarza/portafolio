391861982e4c706366dee4a0aad9841452286a0b
JuanCa <juanca_20201@outlook.es>
2023-11-08 23:56:29 -0500

    pequeños detalles menu

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  isHeaderScrolled = false;
+  isMobileMenu = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY || window.pageYOffset;
+    const scrollThreshold = 100; // Ajusta este valor según tus necesidades
+
+    if (scrollY > scrollThreshold) {
+      this.isHeaderScrolled = true;
+    } else {
+      this.isHeaderScrolled = false;
+    }
+  }
 }
